da13d9e5e5258f01b4c3f9b0e1c2174cbd47a2d6
caio <caiorombaldo@alunos.unicesumar.edu.br>
2023-03-07 20:34:40 -0300

    Manipulação de arquivos - pt1

---
+++ src/atividades/leituraEscrita/Alunos.ts
@@ -0,0 +1,25 @@
+import { writeFile, readFile } from 'fs/promises'
+import { alunos } from "./alunosData"
+
+
+export class Alunos {
+    private dataSource = alunos
+
+    getDataSource(){
+        return this.dataSource
+    }
+
+    writeAlunos(){
+        try{
+            writeFile('alunosDataSource.json', JSON.stringify(this.dataSource, null, 2))
+        }catch(err){
+            console.error('Erro durante a escrita! ', err)
+        }
+    }
+
+    async readAlunosDataSource(){
+        const readAlunos = readFile('alunosDataSource.json', 'utf-8')
+
+        return readAlunos
+    }
+}
---
+++ src/atividades/leituraEscrita/main.ts
@@ -0,0 +1,16 @@
+import { Alunos } from "./Alunos"
+
+
+async function main(){
+    let alunos = new Alunos()
+
+    // alunos.writeAlunos()
+    
+    // const readed = await JSON.parse(alunos.readAlunosDataSource().toString())
+    const readed = JSON.parse(await alunos.readAlunosDataSource())
+    
+    console.log('Listando alunos: ', readed)
+
+}
+
+main()
